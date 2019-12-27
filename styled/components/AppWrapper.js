f14bd4eedde0c02af50558bd4a62426191c18e53
JimmyMcBride <mcbride967@gmail.com>
2019-12-27 09:54:04 -0600

    update

--- styled/components/AppWrapper.js
+++ styled/components/AppWrapper.js
@@ -3,11 +3,6 @@ import theme from "../theme";
 
 // root container for the application
 export default styled.div`
-  min-height: 100vh;
-  max-width: 100vw;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-
+  // text-align: center;
   background: ${theme.gray0};
 `;
