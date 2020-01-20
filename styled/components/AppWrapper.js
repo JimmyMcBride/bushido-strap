82d2f632150accc3c42be3754f24ebe25c489d04
JimmyMcBride <mcbride967@gmail.com>
2020-01-20 11:40:49 -0600

    changed AppWrapper from a div to a Switch from react router

--- styled/components/AppWrapper.js
+++ styled/components/AppWrapper.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import theme from "../theme";
 
+import { Switch } from "react-router-dom";
+
 // root container for the application
-const AppWrapper = styled.div`
+const AppWrapper = styled(Switch)`
   background: ${props =>
     props.background ? props.background : `${theme.gray0}`};
 `;
