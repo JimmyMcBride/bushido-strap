b9ee6c2f0ee7b5fbb823357e1ee19b968db1fd1f
JimmyMcBride <mcbride967@gmail.com>
2020-01-26 17:38:26 -0600

    touch up #7

--- styled/components/maps/styles/boxShadow.js
+++ styled/components/maps/styles/boxShadow.js
@@ -2,6 +2,6 @@ import styledMap from "styled-map";
 import theme from "../../../theme";
 
 export default styledMap`
-  shade: "0 0.3rem 1rem ${theme.gray7}";
+  shade: 0 0.3rem 1rem ${theme.gray7};
   default: none;
 `;
