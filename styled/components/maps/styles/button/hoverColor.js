a15f8f3272ec4d6ee137e07b3a1e41db9c621c15
JimmyMcBride <mcbride967@gmail.com>
2020-01-27 17:27:34 -0600

    button color props reworked touchup #2

--- styled/components/maps/styles/button/hoverColor.js
+++ styled/components/maps/styles/button/hoverColor.js
@@ -2,6 +2,12 @@ import styledMap from "styled-map";
 import theme from "../../../../theme";
 
 export default styledMap`
+  primary: ${theme.gray0};
+  secondary: ${theme.gray0};
+  accent: ${theme.gray0};
+  success: ${theme.gray0};
+  warning: ${theme.gray0};
+  danger: ${theme.gray0};
   invert: ${theme.gray1};
   default: ${theme.gray8};
 `;
