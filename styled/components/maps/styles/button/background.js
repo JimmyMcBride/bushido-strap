b05aa4612e4d1c275a80813e9d2319de9ec69124
JimmyMcBride <mcbride967@gmail.com>
2020-01-27 17:23:40 -0600

    button color props reworked touchup

--- styled/components/maps/styles/button/background.js
+++ styled/components/maps/styles/button/background.js
@@ -2,12 +2,12 @@ import styledMap from "styled-map";
 import theme from "../../../../theme";
 
 export default styledMap`
-  primary: ${theme.blue3}
-  secondary: ${theme.purple3}
-  accent: ${theme.magenta3}
-  success: ${theme.green3}
-  warning: ${theme.orange3}
-  danger: ${theme.red3}
+  primary: ${theme.blue3};
+  secondary: ${theme.purple3};
+  accent: ${theme.magenta3};
+  success: ${theme.green3};
+  warning: ${theme.orange3};
+  danger: ${theme.red3};
   invert: ${theme.gray1};
   default: ${theme.gray8};
 `;
