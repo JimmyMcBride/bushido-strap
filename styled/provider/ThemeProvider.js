5ee07a276044c0e132daf124b5faf4dba17f7daf
JimmyMcBride <mcbride967@gmail.com>
2020-01-31 02:03:04 -0600

    hopeful fix

--- styled/provider/ThemeProvider.js
+++ styled/provider/ThemeProvider.js
@@ -1,4 +1,13 @@
 import { createTheming } from "@callstack/react-theme-provider";
 import theme from "../theme";
 const { ThemeProvider } = createTheming(theme);
-export default ThemeProvider;
+
+const ThemeWrapper = ({ theme, children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+ThemeWrapper.defaultProps = {
+  theme
+};
+
+export default ThemeWrapper;
