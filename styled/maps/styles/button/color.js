c1a07f8a20eb59fa9ef71296ab2bd08a37fefe38
JimmyMcBride <mcbride967@gmail.com>
2020-02-23 00:06:03 -0600

    touch up

--- styled/maps/styles/button/color.js
+++ styled/maps/styles/button/color.js
@@ -2,7 +2,7 @@ import styledMap from "styled-map";
 import theme from "../../../theme";
 
 export default styledMap`
-  // yellow: ${theme.yellow7};
+  yellow: ${theme.yellow8};
   invert: ${theme.gray8};
   default: ${theme.gray0};
 `;
