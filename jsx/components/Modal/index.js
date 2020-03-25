31c1632453e4905423b349983429af2434399681
JimmyMcBride <mcbride967@gmail.com>
2020-03-25 13:10:44 -0500

    reverted changes

--- jsx/components/Modal/index.js
+++ jsx/components/Modal/index.js
@@ -22,7 +22,7 @@ const ModalWrapper = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    // z-index: 1060;
+    z-index: 1040;
     width: 100%;
     height: 100%;
     background-color: ${theme.blackAlpha8};
