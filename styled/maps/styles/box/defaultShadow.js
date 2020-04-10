c7f9693de679770d755dd0c770107a1e67194f09
JimmyMcBride <mcbride967@gmail.com>
2020-04-10 00:20:09 -0500

    fixed typo

--- styled/maps/styles/box/defaultShadow.js
+++ styled/maps/styles/box/defaultShadow.js
@@ -2,7 +2,7 @@ import styledMap from "styled-map";
 import { theme } from "../../../theme";
 
 export default styledMap`
-  shadow: 0 0.3rem 1rem ${theme.gray7};
+  shade: 0 0.3rem 1rem ${theme.gray7};
   backlight: 0 0.3rem 1rem ${theme.gray0};
   default: none;
 `;
